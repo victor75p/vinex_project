5cc101be94256b12e157fc9a12591b49a57e54b0
victor75p <victorojo75@gmail.com>
2024-12-11 13:34:46 +0100

    Add files via upload

---
+++ pspractice.js
@@ -0,0 +1,11 @@
+function myproject() {
+ document.getElementById("demo").innerHTML ="Content is now changed!Congratulations!";
+}     
+
+document.getElementById("demo").innerHTML = 5 + 6;
+
+document.write(5 + 6);
+
+window.alert(5 + 6);
+
+console.log(5 + 6);
